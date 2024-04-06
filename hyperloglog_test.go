0326585784ff26814a33332c4d6c984fb5165f45
hepengzheng <hepengzheng@hotmail.com>
2024-04-06 17:04:37 +0800

    add benchmark

--- hyperloglog_test.go
+++ hyperloglog_test.go
@@ -4,12 +4,18 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHyperLogLog(t *testing.T) {
+var (
+	myhll    = NewHyperLogLog()
+	redishll = NewRedisHLL()
+)
+
+func init() {
 	// Uncomment the following line to regenerate the input set
 	// and this is necessary when you run the test for the first time
 	// note that the generating process may be time-consuming,
@@ -25,28 +31,42 @@ func TestHyperLogLog(t *testing.T) {
 
 	scanner := bufio.NewScanner(file)
 	ctx := context.Background()
-	myhll := NewHyperLogLog()
-	redishll := NewRedisHLL()
+	redishll.rdb.Del(ctx, redisKey)
 	for scanner.Scan() {
 		line := scanner.Text()
 		myhll.Add(ctx, line)
 		redishll.Add(ctx, line)
 	}
 
+}
+
+func Test_MyHLL(t *testing.T) {
+	ctx := context.Background()
 	count := myhll.Count(ctx)
 	fmt.Printf("count: %d\n", count)
 	relativeErr := relativeError(count)
+	assert.True(t, relativeErr < upperBoundRelativeError)
+	fmt.Printf("relative error of my implementation: %f\n", relativeErr)
+}
 
+func Benchmark_MyHLL(b *testing.B) {
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		_ = myhll.Count(ctx)
+	}
+}
+
+func Test_RedisHLL(t *testing.T) {
+	ctx := context.Background()
 	redisCount := redishll.Count(ctx)
 	fmt.Printf("redis count: %d\n", redisCount)
 	redisRelativeErr := relativeError(redisCount)
-
-	t.Cleanup(func() {
-		redishll.rdb.Del(ctx, redisKey)
-	})
-
-	assert.True(t, relativeErr < upperBoundRelativeError)
-
-	fmt.Printf("relative error of my implementation: %f\n", relativeErr)
 	fmt.Printf("relative error of Redis' implementation: %f\n", redisRelativeErr)
+}
+
+func Benchmark_RedisHLL(b *testing.B) {
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		_ = redishll.Count(ctx)
+	}
 }
